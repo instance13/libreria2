5f385886502ed66640932628c0f471e292601c4c
instance13 <graydichotomy@gmail.com>
2024-11-26 12:54:28 -0500

    Libro class updated for today's class!

--- OOP/gestionBiblioteca/src/Libro.java
+++ OOP/gestionBiblioteca/src/Libro.java
@@ -1,3 +1,24 @@
 public class Libro extends itemBiblioteca {
-  
+  private String titulo;
+  private String autor;
+  private int numeroDePaginas;
+
+  public void mostrarInformacion()
+  {
+    System.out.printf("---%nTitulo: %s%nAutor%s%nNumero de páginas: %d%n---%n", this.titulo, this.autor, this.numeroDePaginas);
+  }
+
+  @Override
+  public void prestar() {
+  }
+
+  @Override
+  public void devolver() {
+
+  }
+
+  @Override
+  public void calcularMultas() {
+
+  }
 }
